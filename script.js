fef1f9f54addf3be1e7376835d9de9c7aadc01ea
Chong Yow Lim <yowlimchong2403@gmail.com>
2022-11-08 14:41:17 +0800

    Add styling

--- script.js
+++ script.js
@@ -22,7 +22,13 @@ const displayController = (() => {
     const startButton = document.querySelector("#start-game");
     const restartButton = document.querySelector("#restart");
     const menu = document.querySelector(".menu");
+    const menuForm = document.querySelector(".menu form");
     const overlay = document.querySelector(".overlay");
+    const close = document.querySelector("#close");
+    const congrats = document.querySelector(".congrats");
+    const congratsText = document.querySelector(".congrats-content p");
+    const happyFace = document.querySelector("#win");
+    const mehFace = document.querySelector("#tie");
     const player1 = document.querySelector("#player1");
     const player2 = document.querySelector("#player2");
     const turnText = document.querySelector("#turn");
@@ -34,24 +40,40 @@ const displayController = (() => {
         menu.style.display = "none";
         overlay.style.display = "none";
     };
+    const hideCongrats = () => {
+        mehFace.style.display = "none";
+        happyFace.style.display = "none";
+        congrats.style.display = "none";
+        overlay.style.display = "none";
+    }
     startButton.addEventListener('click', showMenu);
     overlay.addEventListener('click', (e) => {
-        if (e.target !== menu) {
+        if (e.target !== menu && menu.style.display === "inline") {
             hideMenu();
+        } else if (e.target !== congrats && congrats.style.display === "inline") {
+            hideCongrats();
         }
     });
+    close.addEventListener('click', hideCongrats);
     const startGame = () => {
         hideMenu();
         window.startGame(player1.value, player2.value);
-        startButton.style.display = "none";
+        menuForm.reset();
+        startButton.textContent = "Change player names";
         restartButton.style.display = "inline";
     };
 
     const endGame = winner => {
+        congrats.style.display = "inline";
+        overlay.style.display = "inline";
         if (winner) {
             turnText.textContent = `${winner} wins!`;
+            happyFace.style.display = "inline";
+            congratsText.innerHTML = `<strong>${winner}</strong> wins!`;
         } else {
             turnText.textContent = "It's a tie!";
+            mehFace.style.display = "inline";
+            congratsText.textContent = "It's a tie!";
         }
     }
 
@@ -133,6 +155,7 @@ const gameController = ((name1, name2) => {
         }
     }
     const markCell = e => {
+        e.target.style.color = activePlayer.symbol === 'X' ? "red" : 'blue';
         e.target.textContent = activePlayer.symbol;
         let index = parseInt(e.target.getAttribute("data-index"));
         gameBoard.addToArray(activePlayer.symbol, index);
@@ -140,17 +163,14 @@ const gameController = ((name1, name2) => {
 
         checkGameOver(index);
         if (gameOver) {
-            let winner = activePlayer;
-            console.log(`Player ${winner.name} wins!`);
             cells.forEach(item => item.removeEventListener('click', markCell));
             displayController.endGame(activePlayer.name);
         } else if (cellsMarked === 9) {
-            console.log(`It's a tie!`);
             cells.forEach(item => item.removeEventListener('click', markCell));
             displayController.endGame(null);
         } else {
             activePlayer = activePlayer === player1 ? player2 : player1;
-            displayController.turnText.textContent = `${activePlayer.name}'s turn.`;
+            displayController.turnText.textContent = `${activePlayer.name}'s turn. (${activePlayer.symbol})`;
         }
     };
 
@@ -163,7 +183,8 @@ const gameController = ((name1, name2) => {
         gameBoard.resetArray();
         gameOver = false;
         activePlayer = player1;
-        displayController.turnText.textContent = `${player1.name}'s turn.`;
+        displayController.turnText.textContent = `${player1.name}'s turn. (${player1.symbol})`;
+        displayController.restartButton.addEventListener('click', resetGame);
     }
 
     const startGame = () => {
@@ -177,7 +198,7 @@ const gameController = ((name1, name2) => {
 
 function startGame(firstPlayerName, secondPlayerName) {
     let game = gameController(firstPlayerName, secondPlayerName);
-    game.startGame();
+    game.resetGame();
 }
 
 
